7bdbc658284c794a91c017a3cc8ad1ef96de2e04
Richard Miller <richardmmill@gmail.com>
2021-11-01 14:24:50 -0400

    data in Movie Card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+
+    // console.log(props.data)
+
     return (
         <div>
             MovieCard
--- src/components/MovieListing.js
+++ src/components/MovieListing.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { getAllMovies } from '../features/movieSlice'
+import MovieCard from './MovieCard'
+
+import '../styles/MovieListing.scss'
 
 const MovieListing = () => {
     const movies = useSelector(getAllMovies);
-    console.log(movies)
+    // console.log(movies?.Search)
 
     return (
-        <div>
-            MovieListing
-        </div>
+        <section className='movie-list'>
+            {
+                !movies && <div>Loading...</div>
+            }
+            <div className='movie-container'>
+                {
+                    movies?.Search.map(movie => {
+                        return <MovieCard key={movie.title} data={movie} />
+                    })
+                }
+            </div>
+        </section>
     )
 }
 
