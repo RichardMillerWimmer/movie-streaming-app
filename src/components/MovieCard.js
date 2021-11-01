ef6a688587fa22a7bac411eba683aabd93bd5b1c
Richard Miller <richardmmill@gmail.com>
2021-11-01 14:46:26 -0400

    MovieCard build out

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
-const MovieCard = (props) => {
+import '../styles/MovieCard.scss'
 
+const MovieCard = (props) => {
+    const { data } = props;
     // console.log(props.data)
 
     return (
-        <div>
-            MovieCard
+        <div className='card-itam'>
+            <div className='card-inner'>
+                <div className='card-top'>
+                    <img src={data.Poster} alt='movie poster' />
+                </div>
+                <div className='card-info'>
+                    <div className='card-bottom'>
+                        <h4>{data.Title}</h4>
+                        <p>{data.Year}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
