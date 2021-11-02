7e31ee65b94a6df0c3a210d433b4361f0c2825e7
Richard Miller <richardmmill@gmail.com>
2021-11-02 11:57:22 -0400

    fix useParams id, setup MovieDetails

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react'
 import movieApi from '../api/movieApi';
 import { useParams } from 'react-router-dom'
 
+import '../styles/MovieDetails.scss'
+
 const MovieDetails = () => {
     const [details, setDetails] = useState({});
-    const { id } = useParams();
-    console.log(typeof id)
+    const params = useParams();
 
     const fetchDetails = async (id) => {
-        await movieApi.get(`?apiKey=${process.env.REACT_APP_API_KEY}&i=${id}&plot=full`)
+        await movieApi.get(`?apiKey=${process.env.REACT_APP_API_KEY}&i=${params.id}&plot=full`)
             .then((res) => {
                 console.log(res.data);
                 setDetails(res.data)
@@ -18,12 +19,47 @@ const MovieDetails = () => {
 
     useEffect(() => {
         fetchDetails()
-    }, [id])
+    }, [params.id])
 
     return (
-        <div>
-            MovieDetails
-        </div>
+        <section className='movie-section'>
+            <div className='section-left'>
+                <h2 className='movie-title'>{details.Title}</h2>
+                <div className='movie-rating'>
+                    <span>IMDB Rating: {details.imdbRating}</span>
+                    <span>IMDB Votes: {details.imdbVotes}</span>
+                    <span>Runtime: {details.Runtime}</span>
+                    <span>Year: {details.Released}</span>
+                </div>
+                <div className='movie-plot'>{details.Plot}</div>
+                <div className='movie-info'>
+                    <div>
+                        <span>Director:</span>
+                        <p>{details.Director}</p>
+                    </div>
+                    <div>
+                        <span>Stars:</span>
+                        <p>{details.Actors}</p>
+                    </div>
+                    <div>
+                        <span>Genres:</span>
+                        <p>{details.Genre}</p>
+                    </div>
+                    <div>
+                        <span>Languages:</span>
+                        <p>{details.Language}</p>
+                    </div>
+                    <div>
+                        <span>Awards:</span>
+                        <p>{details.Awards}</p>
+                    </div>
+                </div>
+            </div>
+            <div className='section-right'>
+                <img src={details.Poster} alt={details.Title} />
+            </div>
+
+        </section>
     )
 }
 
