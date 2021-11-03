3a2f65951ff0d5079a254df0ebfbd89dc42ac3d6
Richard Miller <richardmmill@gmail.com>
2021-11-03 10:37:35 -0400

    setup slider

--- src/components/MovieListing.js
+++ src/components/MovieListing.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { getAllMovies, getAllSeries } from '../features/movieSlice'
 import MovieCard from './MovieCard'
+import Slider from 'react-slick'
+import { sliderSettings } from '../styles/sliderSettings'
 
 import '../styles/MovieListing.scss'
 
@@ -9,7 +11,7 @@ const MovieListing = () => {
     const movies = useSelector(getAllMovies);
     const series = useSelector(getAllSeries);
 
-    console.log(movies?.Search)
+    // console.log(movies?.Search)
 
     return (
         <section>
@@ -19,20 +21,32 @@ const MovieListing = () => {
             <div className='movie-list'>
                 <h2>Movies</h2>
                 <div className='movie-container'>
-                    {
-                        movies?.Search.map(movie => {
-                            return <MovieCard key={movie.imdbID} data={movie} />
-                        })
-                    }
+                    <Slider {...sliderSettings}>
+                        {
+                            movies?.Search.map(movie => {
+                                return (
+                                    <div key={movie.imdbID}>
+                                        <MovieCard data={movie} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </Slider>
                 </div>
             </div>
             <div className='series-list'>
                 <div className='movie-container'>
-                    {
-                        series?.Search.map(movie => {
-                            return <MovieCard key={movie.title} data={movie} />
-                        })
-                    }
+                    <Slider {...sliderSettings}>
+                        {
+                            series?.Search.map(series => {
+                                return (
+                                    <div key={series.imdbID}>
+                                        <MovieCard data={series} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </Slider>
                 </div>
             </div>
         </section>
